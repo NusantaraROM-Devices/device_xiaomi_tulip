431aa376718183234438cca2d58bf209b7c12a67
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    tulip: Enable freeform windows

--- device.mk
+++ device.mk
@@ -213,6 +213,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
