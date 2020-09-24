da143888129598dbfadd1a01df41020419ca7a8e
Haoran.Wang <elven.wang@nxp.com>
2020-09-24 22:20:16 +0800

    tulip: Grant android.permission.WRITE_DEVICE_CONFIG to gms
    Grant android.permission.WRITE_DEVICE_CONFIG to com.google.android.gms
    package.
    
    TEST: run gts -m GtsDeviceConfigTestCases -t
    com.google.android.deviceconfig.gts.DeviceConfigApiPermissionTests#testWriteDeviceConfigPermission
    
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/13666
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Tian Yang <yang.tian@nxp.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -217,6 +217,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# GMS Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp.permissions-gms.xml
+
 # GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
