7e239ce4edc44b000d481e0bde080a524e7f7768
muti605 <mustafa.sahin605@yandex.com>
2021-11-09 13:37:11 +0300

    tulip: commonize USB
    Signed-off-by: muti605 <mustafa.sahin605@yandex.com>

--- device.mk
+++ device.mk
@@ -370,10 +370,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl:64 \
