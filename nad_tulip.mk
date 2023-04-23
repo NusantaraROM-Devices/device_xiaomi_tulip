0169864df3a2923c72d90133b055399b84a8199f
Rafiester <rafi.haqqi@gmail.com>
2023-04-23 16:11:29 +0700

    tulip: NusantaraProject Bringup
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- nad_tulip.mk
+++ nad_tulip.mk
@@ -8,14 +8,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
 
+# Inherit some common NusantaraProject stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Extra Flags
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGING := true
+TARGET_BUILD_WITH_LTO := true
+
 # Device Info
-PRODUCT_NAME := evolution_tulip
+PRODUCT_NAME := nad_tulip
 PRODUCT_DEVICE := tulip
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 6 Pro
@@ -23,9 +30,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Evolution-X things
-EVO_BUILD_TYPE := OFFICIAL
